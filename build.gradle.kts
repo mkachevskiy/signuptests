142858447e38c742894215adf0741c0ac8fc66c0
mkachevskiy <mkachevskiy@gmail.com>
2022-06-08 19:42:55 +0300

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,17 +10,17 @@ repositories {
 }
 
 dependencies {
-    implementation("com.codeborne:selenide:6.2.1")
+    implementation("com.codeborne:selenide:6.4.0")
 
     testImplementation("com.github.javafaker:javafaker:1.0.2")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
-    testImplementation("io.qameta.allure:allure-junit5:2.17.2")
+    testImplementation("io.qameta.allure:allure-junit5:2.17.3")
 
-    testRuntimeOnly("org.slf4j:slf4j-simple:1.7.35")
+    testRuntimeOnly("org.slf4j:slf4j-simple:1.7.36")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 
-    compileOnly ("org.projectlombok:lombok:1.18.22")
+    compileOnly ("org.projectlombok:lombok:1.18.24")
     annotationProcessor ("org.projectlombok:lombok:1.18.22")
 }
 
