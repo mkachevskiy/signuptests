67746bb1f4599251ee8ace23be241ed4eca67903
mkachevskiy <mkachevskiy@gmail.com>
2023-04-06 22:15:13 +0300

    Update libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,13 +10,13 @@ repositories {
 }
 
 dependencies {
-    implementation("com.codeborne:selenide:6.10.1")
+    implementation("com.codeborne:selenide:6.12.3")
     implementation("commons-io:commons-io:2.11.0")
 
     testImplementation("com.github.javafaker:javafaker:1.0.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.0")
-    testImplementation("io.qameta.allure:allure-junit5:2.20.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.2")
+    testImplementation("io.qameta.allure:allure-junit5:2.21.0")
 
     testRuntimeOnly("org.slf4j:slf4j-simple:2.0.5")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
